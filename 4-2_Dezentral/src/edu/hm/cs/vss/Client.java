68b4a8c4d800fc127653816469aebf04991db99e
Michael Freiwald <m.freiwald@icloud.com>
2015-05-22 11:23:24 +0200

    Teste Client zugriffe.

--- 4-2_Dezentral/src/edu/hm/cs/vss/Client.java
+++ 4-2_Dezentral/src/edu/hm/cs/vss/Client.java
@@ -6,21 +6,21 @@ import java.rmi.server.UnicastRemoteObject;
 public class Client extends UnicastRemoteObject implements IClient {
 
 	private static final long serialVersionUID = 1L;
-	private Client left1;
-	private Client left2;
-	private Client right1;
-	private Client right2;
+	private IClient left1;
+	private IClient left2;
+	private IClient right1;
+	private IClient right2;
 	
 	protected Client() throws RemoteException {
 		super();
 	}
 	
 	@Override
-	public void setLeft(Client newLeft) {
+	public void setLeft(IClient newLeft) throws RemoteException {
 		this.left1 = newLeft;
 		
 		// sag ihm du bist der neue rechte
-		Client newRight = this.left1.ichBinDeinNeuerRechter(this);
+		IClient newRight = this.left1.ichBinDeinNeuerRechter(this);
 		
 		if (newRight != null) {
 			this.right1 = newRight;
@@ -44,8 +44,8 @@ public class Client extends UnicastRemoteObject implements IClient {
 			this.right2.findNeighbours();
 	}
 	
-	private Client ichBinDeinNeuerRechter(Client newRight) {
-		Client oldRight = this.right1;
+	public IClient ichBinDeinNeuerRechter(IClient newRight) {
+		IClient oldRight = this.right1;
 		this.right1 = newRight;
 		if(this.left1 == null) {
 			this.left1 = this.right1;
@@ -54,8 +54,8 @@ public class Client extends UnicastRemoteObject implements IClient {
 		return oldRight;
 	}
 	
-	private Client ichBinDeinNeuerLinker(Client newLeft) {
-		Client oldLeft = this.left1;
+	public IClient ichBinDeinNeuerLinker(IClient newLeft) {
+		IClient oldLeft = this.left1;
 		this.left1 = newLeft;
 		if(this.right1 == null) {
 			this.right1 = this.left1;
@@ -65,23 +65,30 @@ public class Client extends UnicastRemoteObject implements IClient {
 	}
 	
 	
-	private void findNeighbours() {
-		
+	public void findNeighbours() {
+		Logging.log(Logger.Client, "Suche deine Nachbarn..");
 		if(left1 != null)
-			left2 = left1.left1;
+			left2 = left1.getLeft1();
 				
 		if(left2 == this) {
 			left2 = null;
 		}
 		
 		if(right1 != null) 
-			right2 = right1.right1;
+			right2 = right1.getRight1();
 				
 		if(right2 == this) {
 			right2 = null;
 		}
 		
 		
+	}
+	
+	public IClient getLeft1() {
+		return this.left1;
+	}
+	public IClient getRight1() {
+		return this.right1;
 	}
 	
 }
--- 4-2_Dezentral/src/edu/hm/cs/vss/IClient.java
+++ 4-2_Dezentral/src/edu/hm/cs/vss/IClient.java
@@ -5,6 +5,11 @@ import java.rmi.RemoteException;
 
 public interface IClient extends Remote {
 	
-	public void setLeft(Client newLeft) throws RemoteException;
+	public void setLeft(IClient newLeft) throws RemoteException;
+	IClient ichBinDeinNeuerRechter(IClient newRight) throws RemoteException;
+	IClient ichBinDeinNeuerLinker(IClient newLeft) throws RemoteException;
+	void findNeighbours() throws RemoteException;
 	
+	IClient getLeft1();
+	IClient getRight1();
 }
